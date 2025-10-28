9539af7a2a5ce2712759042035234d805cc5f196
MuhammadShidqulIman <muhammadshidqul3@gmail.com>
2025-10-28 10:58:49 +0700

    Mengubah Route delete

--- index.js
+++ index.js
@@ -61,17 +61,18 @@ app.put("/komik/:id", async (req, res) => {
 app.delete('/komik/:id', async (req, res) => {
     const id = req.params.id;
     try {
-        const komik = await db.komik.findBypk(id);
+        const komik = await db.Komik.findByPk(id); 
         if (!komik) {
-            return res.status(404).send({ message: 'komik tidak ditemukan '});
+            return res.status(404).send({ message: 'Komik tidak ditemukan' });
         }
 
         await komik.destroy();
-        res.send({ message: 'komik berhasil dihapus '});
+        res.send({ message: 'Komik berhasil dihapus' });
     } catch (err) {
         res.status(500).send(err);
     }
-})
+});
+
 
 
 
