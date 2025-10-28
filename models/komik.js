0f7601e98162f8a8bbecf6faccf523ebd57ffdc4
MuhammadShidqulIman <muhammadshidqul3@gmail.com>
2025-10-28 09:51:40 +0700

    tambah model Komik

---
+++ models/komik.js
@@ -0,0 +1,19 @@
+module.exports = (sequelize, DataTypes) => {
+    const komik = sequelize.define("Komik", {
+        id:{
+            type: DataTypes.INTEGER,
+            autoIncrement: true,
+            primarykey: true
+        },
+        title:{
+            type: DataTypes.STRING,
+        },
+        description: {
+            type: DataTypes.STRING,
+        },
+        author: {
+            type: DataTypes.STRING,
+        }
+    });
+    return Komik
+}
